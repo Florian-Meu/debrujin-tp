68fef3f9d0743804a952030551c88184987ac848
Florian-Meu.git <florian.meurisse@outlook.fr.git>
2019-10-22 12:19:29 +0200

    first commit

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -14,9 +14,23 @@ default=21,
 help='taille des kmer (optionnel - par defaut:21)')
 	parser.add_argument('-r',
 action='store_true',
-default='',
+default='')
 	args = parser.parse_args()
 
 def read_fastq(nom_fichier_fastq):
-	
+	lire=False
+	with open(nom_fichier_fastq) as fastq :
+		for ligne in fastq :
+			if ligne.startswith("@"):
+				lire=True
+			elif ligne.startswith("+"):
+				lire=False
+			else :
+				if lire :
+					yield ligne
 
+for sequence in read_fastq(args.i):
+	print (sequence)
+
+if __name__ == "__main__":
+	main()
